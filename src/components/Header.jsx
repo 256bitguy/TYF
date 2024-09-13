fbbd58634c2972871826607de3ba12497941efd7
67vivekraj <vivekraj.srivastava@linktoany.com>
2024-09-13 23:59:19 +0530

    routing stuffs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,15 +56,18 @@ export default function Header() {
                   <div className="ml-10 flex items-baseline space-x-4">
                     {navigation.map((item) => (
                       <Link
-                        key={item.name}
-                        href={item.href}
-                        aria-current={item.current ? 'page' : undefined}
-                        className={classNames(
+                       to={item.href}
+                       >
+                        <h1
+                         key={item.name}
+                         aria-current={item.current ? 'page' : undefined}
+                         className={classNames(
                           item.current ? 'bg-purple-900 text-white' : 'text-purple-300 hover:bg-purple-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium',
-                        )}
-                      >
+                        )}>
                         {item.name}
+                        </h1>
+                        
                       </Link>
                     ))}
                   </div>
